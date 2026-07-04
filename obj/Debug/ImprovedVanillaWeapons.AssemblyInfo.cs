07584579268de40e3ab17cd358d6b06ac10e2672
NuTrynity <88869373+NuTrynity@users.noreply.github.com>
2026-07-04 14:37:33 +0800

    changed setting text to be more clarifying

--- obj/Debug/ImprovedVanillaWeapons.AssemblyInfo.cs
+++ obj/Debug/ImprovedVanillaWeapons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ImprovedVanillaWeapons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33a4984e9b1983f8e0f7a4a9a38e9010386ef637")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8bac8a1ce2bfb5b3bc87687d5673c2a2ade2cf1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ImprovedVanillaWeapons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ImprovedVanillaWeapons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
